6257936e7f986d294ea427ea2f9713ed052785b2
bMacSwigg <brian.mcswiggen@gmail.com>
2018-03-21 19:18:00 -0400

    Modifications to module
    The axioms should now be complete. There are a couple of lingering
    questions about the return values of set and insert, about key_rel, and
    about the form of cursor_order.

--- model/BTreesModule.v
+++ model/BTreesModule.v
@@ -11,9 +11,9 @@ Module Type CURSOR_TABLE.
  Parameter empty_t: table.
 
  Parameter make_cursor: key -> table -> cursor.
- Parameter key_rel: key -> cursor -> bool.
+ Parameter get_key: cursor -> option key.
  Parameter get: cursor -> option V.
- Parameter set: cursor -> V -> table.
+ Parameter set: cursor -> V -> table. (* Should these return cursors instead? Since cursor holds all info of the table *)
  Parameter insert: cursor -> key -> V -> table.
  Parameter next: cursor -> cursor.
  Parameter prev: cursor -> cursor.
@@ -21,23 +21,29 @@ Module Type CURSOR_TABLE.
  Parameter last_cursor: table -> cursor.
 
  Parameter abs_rel: table -> cursor -> Prop. (* Relationship between table and cursor *)
+ Parameter key_rel: key -> cursor -> Prop. (* Is the key in this cursor's range? *)
+  (* get_key c >= k /\ get_key (prev c) < k, with special cases for first_cursor and last_cursor *)
 
  Axiom make_cursor_rel: forall t k,
        abs_rel t (make_cursor k t).
 
- Axiom glast: forall t,                (* get-empty *)
+ Axiom glast: forall t,        (* get-last *)
        get (last_cursor t) = None.
- Axiom gss: forall k v t,      (* get-set-same *)
-       get (make_cursor k (set (make_cursor k t) v)) = Some v.
- Axiom gso: forall j k v t,    (* get-set-other *)
-       j <> k -> get (make_cursor j (set (make_cursor k t) v)) = get (make_cursor j t).
- Axiom gis: forall k v t,      (* get-insert-same *)
-       get (make_cursor k (insert (make_cursor k t) k v)) = Some v.
- Axiom gio: forall j k v t,    (* get-insert-other *)
-       j <> k -> get (make_cursor j (insert (make_cursor k t) k v)) = get (make_cursor j t).
+ Axiom gss: forall k v c,      (* get-set-same *)
+       get_key c = Some k -> get (make_cursor k (set c v)) = Some v.
+ Axiom gso: forall j v c t,    (* get-set-other *)
+       abs_rel t c -> ~ key_rel j c -> get (make_cursor j (set c v)) = get (make_cursor j t).
+ Axiom gis: forall k v c,      (* get-insert-same *)
+       key_rel k c -> get (make_cursor k (insert c k v)) = Some v.
+ Axiom gio: forall j k v c t,    (* get-insert-other *)
+       key_rel k c -> ~ key_rel j c -> abs_rel t c -> get (make_cursor j (insert c k v)) = get (make_cursor j t).
 
- Axiom next_prev: forall c t ,
+ Axiom next_prev: forall c t,
        abs_rel t c -> ~ (c = last_cursor t) -> prev (next c) = c.
+ Axiom prev_next: forall c t,
+       abs_rel t c -> ~ (c = first_cursor t) -> next (prev c) = c.
+ Axiom cursor_order: forall c k1 k2, (* I don't love the use of lt_key here *)
+       get_key c = Some k1 -> get_key (next c) = Some k2 -> lt_key k1 k2 = true.
 End CURSOR_TABLE.
 
 (* NOT up to date *)
