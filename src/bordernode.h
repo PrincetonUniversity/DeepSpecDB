f2ab04377f31a56495a9b172695b74ad345f9f52
Yixuan Chen <yixuanc@princeton.edu>
2018-05-22 11:18:21 -0400

    refactor the bordernode, breaking the kvstore

--- src/bordernode.h
+++ src/bordernode.h
@@ -4,7 +4,7 @@
  * and open the template in the editor.
  */
 
-/* 
+/*
  * File:   bordernode.h
  * Author: Oluwatosin V. Adewale
  *
@@ -14,34 +14,34 @@
 #ifndef BORDERNODE_H
 #define BORDERNODE_H
 
-/* A BorderNode hold keys and their values and/or points to a Relation 
+#include <stddef.h>
+#include "util.h"
+
+/* A BorderNode hold keys and their values and/or points to a Relation
  * indexing the next slice of keys with similar prefixes. */
 typedef struct BorderNode* BorderNode_T;
 
-/* Create a new BorderNode. Returns the new BorderNode if successful, otherwise
- * returns NULL. All values and the suffix are set to NULL. */
-BorderNode_T BN_NewBorderNode(unsigned long keyslice);
+/* Create a new BorderNode.
+ * TODO: This function invokes [surely_malloc], which will exit when malloc fail */
+BorderNode_T BN_NewBorderNode();
 
 /* Free a BorderNode.*/
 void BN_FreeBorderNode(BorderNode_T bordernode);
 
-/* Get the keySlice associated with this bordernode. */
-unsigned long BN_GetKeySlice(BorderNode_T bn);
-
-/* Set the value of the key at idx i. i must be between 0 and 9. */
-void BN_SetValue(BorderNode_T bn, int i, void* val);
+/* Set the value of the key at idx i, where i \in [0, keyslice_length). */
+void BN_SetPrefixValue(BorderNode_T bn, int i, void* val);
 
 /* Get the value of the key. */
-void* BN_GetValue(BorderNode_T bn, int i);
+void* BN_GetPrefixValue(BorderNode_T bn, int i);
 
-/* Set the suffix for key at idx 9 and its length.*/
-void BN_SetSuffix(BorderNode_T bn, char* suf, size_t len);
+/* Set the suffix key, length and value.*/
+void BN_SetSuffixValue(BorderNode_T bn, char* suf, size_t len, void *val);
 
-/* Get the suffix of the key at idx 9. */
-char* BN_GetSuffix(BorderNode_T bn);
+/* Test whether the bordernode has a value corresponding to a suffix. */
+Bool BN_HasSuffix(BorderNode_T bn);
 
-/* Get the length of the suffix of the key at idx 9. */
-size_t BN_GetSuffixLength(BorderNode_T bn);
-
+/* Test whether the suffix is the same as the given one,
+ * return value if successful, NULL otherwise */
+void *BN_GetSuffixValue(BorderNode_T bn, char *suf, size_t len);
 
 #endif /* BORDERNODE_H */
