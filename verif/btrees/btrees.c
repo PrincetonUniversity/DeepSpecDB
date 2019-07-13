699c79d4946c26200b5c22644551890a1bfd06b2
Pablo Le Hénaff <pablo.le-henaff@polytechnique.edu>
2019-07-14 01:11:27 +0200

    Removed structures and unions so that clightgen is happy

--- verif/btrees/btrees.c
+++ verif/btrees/btrees.c
@@ -1,4 +1,4 @@
-// Authors: Oluwatosin V. Adewale, Aurèle Barrière, Pablo Le Hénaff 
+// Authors: Pablo Le Hénaff, Oluwatosin V. Adewale, Aurèle Barrière
 
 #include <assert.h>
 #include <stdlib.h>
@@ -11,24 +11,20 @@
 
 typedef size_t key;
 typedef struct node node;
+
 struct btree {
   struct node* root;
-  size_t num_records;
+  unsigned int num_records;
   unsigned int depth;
 };
 
-typedef union child_or_record {
-  node* child;
-  const void* record;
-} child_or_record;
-
 struct entry {
   key key;
-  union child_or_record content;
+  void* ptr;
 };
 
 struct node {
-  unsigned int is_leaf: 1;
+  unsigned char is_leaf; // or bit field
   unsigned int num_keys;
   node* ptr0;
   struct entry entries[2*PARAM + 1]; // extra space for insertion before splitting
@@ -46,7 +42,7 @@ typedef struct cursor {
 } cursor;
 
 struct btree* new_btree() {
-  struct btree* t;
+  struct btree *t;
   if(! (t = malloc(sizeof(struct btree)))) exit(1);
   if(! (t->root = malloc(sizeof(node)))) exit(1);
   t->root->is_leaf = 1;
@@ -57,20 +53,16 @@ struct btree* new_btree() {
   return t;
 };
 
-node* child_at(node *n, int i) {
-  if(i < 0) return n->ptr0;
-  else return n->entries[i].content.child;
-};
-
-const void* record_at(node *n, int i) {
-  return n->entries[i].content.record;
+void* ptr_at(node *n, int i) {
+  if(i < 0) return (void*) n->ptr0;
+  else return n->entries[i].ptr;
 };
 
 void down_to_first(cursor *c) {
   while(!c->ancestors[c->level].node->is_leaf) {
     struct cursor_entry last = c->ancestors[c->level];
     c->level++;
-    c->ancestors[c->level].node = child_at(last.node, last.index);
+    c->ancestors[c->level].node = (node*) ptr_at(last.node, last.index);
     c->ancestors[c->level].index = c->ancestors[c->level].node->is_leaf ? 0 : -1;
   };
 };
@@ -121,7 +113,7 @@ void down_to_key(cursor *c, key k) {
   while(!c->ancestors[c->level].node->is_leaf) {
     struct cursor_entry last = c->ancestors[c->level];
     c->level++;
-    c->ancestors[c->level].node = child_at(last.node, last.index);
+    c->ancestors[c->level].node = (node*) ptr_at(last.node, last.index);
     c->ancestors[c->level].index = find_index(c->ancestors[c->level].node, k);
   };
 };
@@ -133,22 +125,32 @@ void move_to_key(cursor *c, key k) {
   return;
 };
 
-const void* get(cursor *c, key k) {
+void* get(cursor *c, key k) {
   move_to_key(c, k);
-  return record_at(c->ancestors[c->level].node, c->ancestors[c->level].index);
+  return ptr_at(c->ancestors[c->level].node, c->ancestors[c->level].index);
 };
 
-void insert_entry(node *n, unsigned int i, key k, child_or_record data) {
+void init(cursor *c) {
+  move_to_first(c);
+};
+
+void* next(cursor *c) {
+  void *rec = ptr_at(c->ancestors[c->level].node, c->ancestors[c->level].index);
+  move_to_next(c);
+  return rec;
+};
+
+void insert_entry(node *n, unsigned int i, key k, void* ptr) {
   int c;
   for(c = n->num_keys; c > i; c--)
     n->entries[c] = n->entries[c-1]; // shift
     
   n->entries[i].key = k;
-  n->entries[i].content = data;
+  n->entries[i].ptr = ptr;
   n->num_keys++;
 };
 
-void insert_aux(cursor *c, int level, key k, child_or_record data) {
+void insert_aux(cursor *c, int level, key k, void* ptr) {
   if(level <= -1) { // update the cursor too
     node *new_root;
     if(! (new_root = malloc(sizeof(node)))) exit(1);
@@ -157,7 +159,7 @@ void insert_aux(cursor *c, int level, key k, child_or_record data) {
     node *old_root = c->btree->root;
     new_root->ptr0 = old_root;
     new_root->entries[0].key = k;
-    new_root->entries[0].content = data;
+    new_root->entries[0].ptr = ptr;
     c->btree->root = new_root;
     c->btree->num_records++;
     c->btree->depth++;
@@ -172,61 +174,50 @@ void insert_aux(cursor *c, int level, key k, child_or_record data) {
 
   int i = c->ancestors[level].index;
   node *n = c->ancestors[level].node;
-  
+
   if(n->is_leaf) {
-    if(i < n->num_keys && n->entries[i].key == k) n->entries[i].content = data; // update, i >= 0
+    if(i < n->num_keys && n->entries[i].key == k) n->entries[i].ptr = ptr; // update
     else if(n->num_keys < 2*PARAM) {
-	insert_entry(n, i, k, data);
-	c->btree->num_records++;
+      insert_entry(n, i, k, ptr);
+      c->btree->num_records++;
     } else {
-      insert_entry(n, i, k, data);
+      insert_entry(n, i, k, ptr);
       // split the leaf node
-      node *new;
+      node* new;
       if(! (new = malloc(sizeof(struct node)))) exit(1);
       new->ptr0 = NULL;
       new->is_leaf = 1;
       memcpy(new->entries, n->entries + PARAM, (PARAM + 1) * sizeof(struct entry));
       new->num_keys = PARAM + 1;
       n->num_keys = PARAM;
-      return insert_aux(c, level - 1, new->entries[0].key, (child_or_record) new);
+      insert_aux(c, level - 1, new->entries[0].key, (void*) new);
     };
   } else {
     if(n->num_keys < 2*PARAM) {
-      insert_entry(n, i + 1, k, data);
+      insert_entry(n, i + 1, k, ptr);
       c->btree->num_records++;
     } else {
-      insert_entry(n, i + 1, k, data);
+      insert_entry(n, i + 1, k, ptr);
       node *new;
       if(! (new = malloc(sizeof(struct node)))) exit(1);
       key middle_key = n->entries[PARAM].key;
-      new->ptr0 = n->entries[PARAM].content.child;
+      new->ptr0 = (node*) n->entries[PARAM].ptr;
       new->is_leaf = 0;
       memcpy(new->entries, n->entries + PARAM + 1, (PARAM + 1) * sizeof(struct entry));
       new->num_keys = PARAM;
       n->num_keys = PARAM;
-      return insert_aux(c, level - 1, middle_key, (child_or_record) new);
+      insert_aux(c, level - 1, middle_key, (void*) new);
     };
   };
+
   return;
 };
 
-void insert(cursor *c, key k, const void* rec) {
+void insert(cursor *c, key k, void* ptr) {
   move_to_key(c, k);
-  insert_aux(c, c->level, k, (child_or_record) rec);
+  insert_aux(c, c->level, k, ptr);
 };
 
 int main() {
-
-  struct btree *t = new_btree();
-  cursor *c = new_cursor(t);
-
-  int k;
-  for(k = 0; k < 30; k++) insert(c, k, NULL);
-  for(k = 29; k >= 0; k--) insert(c, k, k%2==0 ? NULL : 1);
-
-  
-
-  printf("%p\n", get(c, 23));
-
   return 0;
 };
