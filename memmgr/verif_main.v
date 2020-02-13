685a9381a4ad9c2014bab4534c5a16267186c887
Andrew W. Appel <appel@princeton.edu>
2020-02-13 13:54:57 -0500

    Tweaked verif_main.v

--- memmgr/verif_main.v
+++ memmgr/verif_main.v
@@ -3,6 +3,7 @@ Require Import linking.
 Require Import malloc.
 (* Require Import mmap0. *)
 Require Import main.
+Require Import malloc_lemmas.
 Require Import spec_malloc.
 Require Import spec_main.
 
@@ -10,13 +11,19 @@ Definition Gprog : funspecs := spec_main.specs ++ user_specs_R ++ private_specs.
 
 Definition Vprog : varspecs. mk_varspecs linked_prog. Defined. 
 
+
+Lemma create_mem_mgr_R: 
+  forall (gv: globals),
+  !! (headptr (gv malloc._bin)) &&
+   data_at_ Ews (tarray (tptr tvoid) BINS) (gv malloc._bin)
+     |-- mem_mgr_R gv emptyResvec.
+Admitted.
+
 Lemma body_main: semax_body Vprog Gprog f_main main_spec.
 Proof.
 start_function.  (* was formerly very slow, fixed now. *)
-(*
-sep_apply (create_mem_mgr gv).
-assert (change_composite_env spec_onepile.CompSpecs CompSpecs).
-*)
+change 8 with BINS.
+sep_apply (create_mem_mgr_R gv); auto.
 admit.
 all:fail.
 Admitted.
