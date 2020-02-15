79efa2bb9087e28ba77886b834e6f8ad7c3de86c
Dave Naumann <naumann@cs.stevens.edu>
2020-02-15 17:50:03 -0500

    add main_R and its verification; uses pre_fill

---
+++ memmgr/verif_main_R.v
@@ -0,0 +1,61 @@
+Require Import VST.floyd.proofauto.
+Require Import linking.
+Require Import malloc.
+Require Import main_R.
+Require Import malloc_lemmas.
+Require Import spec_malloc.
+
+
+Definition linked_prog : Clight.program :=
+ ltac: (linking.link_progs_list [malloc.prog; main_R.prog]).
+
+Instance CompSpecs : compspecs. make_compspecs linked_prog. Defined.
+
+Definition Vprog : varspecs. mk_varspecs linked_prog. Defined.
+
+Local Open Scope assert.
+
+Definition main_spec :=
+ DECLARE _main
+ WITH gv: globals
+ PRE [ ] main_pre linked_prog tt nil gv
+ POST[ tint ]
+    PROP()
+    LOCAL(temp ret_temp (Vint (Int.repr 0)))
+    SEP(TT).
+
+Definition Gprog : funspecs := [main_spec] ++ external_specs ++ user_specs_R.
+
+Lemma body_main: semax_body Vprog Gprog f_main main_spec.
+Proof.
+start_function.
+change 8 with BINS.
+sep_apply (create_mem_mgr_R gv); auto.
+forward_call BIGBLOCK.  (*! *m = mmap0(BIGBLOCK ...) !*)  
+rep_omega.
+Intros m.
+forward_if.
+{ if_tac; entailer!. }
++ (* m = null *)
+  forward. (* return *)
++ (* m <> null *)
+  if_tac. contradiction.
+  forward_call(50,m,gv,emptyResvec). (* pre_fill(50,m) *)
+  split.  rep_omega. auto.
+  set (rvec:=add_resvec emptyResvec (size2binZ 50) (chunks_from_block (size2binZ 50))).
+  forward_call(50,gv,rvec). (* t1 = malloc(50) *)
+(* TODO unfortunately it used malloc_spec_R - we'd like to use malloc_spec_R_simple *)
+  rep_omega.
+  Intros p.
+  change (guaranteed rvec 50) with true.
+  cbv iota.
+  assert_PROP (p <> nullval) by entailer!.
+  forward_call(50,p,gv,(add_resvec rvec (size2binZ 50) (-1))). (* free(p) *)
+  rewrite if_false by auto.
+  entailer!.
+  forward.
+Qed.
+
+
+
+
