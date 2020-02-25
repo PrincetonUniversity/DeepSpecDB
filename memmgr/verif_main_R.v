4291d77f87ae5674e37413ee787cd193d5623356
Dave Naumann <naumann@cs.stevens.edu>
2020-02-25 09:33:48 -0500

    Alt proof of main_R using the user-friendly resourced spec

--- memmgr/verif_main_R.v
+++ memmgr/verif_main_R.v
@@ -43,8 +43,7 @@ forward_if.
   forward_call(50,m,gv,emptyResvec). (* pre_fill(50,m) *)
   split.  rep_omega. auto.
   set (rvec:=add_resvec emptyResvec (size2binZ 50) (chunks_from_block (size2binZ 50))).
-  forward_call(50,gv,rvec). (* t1 = malloc(50) *)
-(* TODO unfortunately it used malloc_spec_R - we'd like to use malloc_spec_R_simple *)
+  forward_call(50,gv,rvec). (* t1 = malloc(50) --- using malloc_spec_R *)
   rep_omega.
   Intros p.
   change (guaranteed rvec 50) with true.
@@ -55,6 +54,43 @@ forward_if.
   entailer!.
   forward.
 Qed.
+
+
+(* Proof of main using the malloc_spec_R_simple *)
+
+Definition user_specs_R' := 
+[pre_fill_spec'; try_pre_fill_spec'; malloc_spec_R_simple'; free_spec_R'].
+
+Definition Gprog' : funspecs := [main_spec] ++ external_specs ++ user_specs_R'.
+
+Lemma body_main': semax_body Vprog Gprog' f_main main_spec.
+Proof.
+start_function.
+change 8 with BINS.
+sep_apply (create_mem_mgr_R gv); auto.
+forward_call BIGBLOCK.  (*! *m = mmap0(BIGBLOCK ...) !*)  
+rep_omega.
+Intros m.
+forward_if.
+{ if_tac; entailer!. }
++ (* m = null *)
+  forward. (* return *)
++ (* m <> null *)
+  if_tac. contradiction.
+  forward_call(50,m,gv,emptyResvec). (* pre_fill(50,m) *)
+  split.  rep_omega. auto.
+  set (rvec:=add_resvec emptyResvec (size2binZ 50) (chunks_from_block (size2binZ 50))).
+  forward_call(50,gv,rvec). (* t1 = malloc(50) *)
+  split. rep_omega.  
+  change (guaranteed rvec 50) with true; auto.
+  Intros p.
+  assert_PROP (p <> nullval) by entailer!.
+  forward_call(50,p,gv,(add_resvec rvec (size2binZ 50) (-1))). (* free(p) *)
+  rewrite if_false by auto.
+  entailer!.
+  forward.
+Qed.
+
 
 
 
