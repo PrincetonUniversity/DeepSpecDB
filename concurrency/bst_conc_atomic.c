1ba0fb0c93a427d87785c8dee4f54c2c936a322e
Roshan Sharma <rossharma1@gmail.com>
2020-05-27 22:02:35 -0500

    Comleted implementation of addonly non-blocking concurrent binary search treecu
    - remove is still pending
    - treebox free is also sequential version

--- concurrency/bst_conc_atomic.c
+++ concurrency/bst_conc_atomic.c
@@ -4,13 +4,20 @@
 #include "threads.h"
 #include <stdatomic.h>
 
+#define NULL 0
+
+#define N 200
 
 extern void *malloc (size_t n);
 extern void free(void *p);
 
-typedef struct tree {int key; void *value; _Atomic struct tree *left, *right;} tree;
+typedef struct tree {int key; _Atomic void **value; _Atomic struct tree **left, **right;} tree;
 typedef _Atomic struct tree **treebox;
+typedef _Atomic void **valuepointer;
 treebox tb ;
+
+lock_t thread_lock[N];
+
 
 void *surely_malloc (size_t n) {
   void *p = malloc(n);
@@ -20,59 +27,59 @@ void *surely_malloc (size_t n) {
 
 treebox treebox_new(void) {
   treebox p = (treebox) surely_malloc(sizeof (*p));
-  *p = NULL;
+  atomic_store(p,NULL);
   return p;
 }
 
+void treebox_free(treebox b) {
+  struct tree *t = *b;
+  if(t != NULL){  
+    treebox_free(t->right);    
+    treebox_free(t->left);
+    free(t->value);
+    free(t);
+  }
+  free(b);
 
-// void tree_free(struct tree *tp) {
-//   struct tree *tpa, *tpb;
-//   tpa=tp->left;
-//   tpb=tp->right;
-//   free(tp);
-//   if (tpa!=NULL) {
-//     tree_free(tpa);
-//   }
-//  if (tpb!=NULL) {
-//     tree_free(tpb);
-//   }
-// }
+}
 
-// void treebox_free(treebox b) {
-//   struct tree *t = *b;
-//   if(t != NULL){  
-//    tree_free(t);
-//   }
-//   free(b);
-// }
 
 void insert (treebox tb, int x, void *value) {
+  struct tree *t ;
   for(;;) {
-       _Atomic struct tree *t = atomic_load(tb);
-      if (t==NULL) {         
+      t = atomic_load(tb);
+      if (t==NULL) {
       struct tree *p  = (struct tree *) surely_malloc (sizeof *p);
       p->key=x;
-      p->value=value;
-      p->left= NULL;
-      p->right=NULL;
+      valuepointer v =  (valuepointer) surely_malloc (sizeof *v);
+      atomic_store(v,value);
+      p->value=v;
+      treebox lp = (treebox) surely_malloc(sizeof (*p));
+      *lp = NULL;
+      p->left  =  lp;
+      treebox rp = (treebox) surely_malloc(sizeof (*p));
+      *rp = NULL;
+      p->right = rp;
       int result = atomic_compare_exchange_strong(tb, &t, p);
       if(result){
         return;
       }
       else{
+        free(v);
+        free(lp);
+        free(rp);
         free(p);        
         continue;
       }
+      return;
     } else {
-      struct tree p = atomic_load(t);
-      int y = p.key;
+      int y = t->key;
       if (x<y){
-       tb = &p.left;
+       tb= t->left;
       } else if (y<x){
-       tb = &p.right;       
+       tb = t->right;       
       }else {
-      	p.value=value;
-        atomic_store(t,p);
+        atomic_store(t->value,value);
       	return;
       }
     }
@@ -81,129 +88,75 @@ void insert (treebox tb, int x, void *value) {
 
 
 void *lookup (treebox tb, int x) {
-  _Atomic struct tree *t = atomic_load(tb);
+   struct tree *t = atomic_load(tb);
    void *v;
   while (t!=NULL) {
-     struct tree p = atomic_load(t);
-    int y = p.key;
+    int y = t->key;
     if (x<y){
-      t= p.left;
+      t= atomic_load(t->left);
     }else if (y<x){
-      t=p.right;
+      t= atomic_load(t->right);
     }else {
-      v = p.value;
+      v = atomic_load(t->value);
       return v;
     }
   }
-  return NULL;
+  return "value not found";
 }
 
-// void turn_left(struct tree_t * tgl, struct tree_t * tgr) {
-//   struct tree_t * mid;
-//   struct tree * r, *l;
-//   r = tgr->t;
-//   mid = r->left;
-//   l = tgl->t;
-//   l->right = mid;
-//   r->left = tgr; 
-//   tgl->t = r;
-//   tgr->t = l;
-// }
-
-// void pushdown_left (struct tree_t *tgp) {
-//   struct tree *p, *q;
-//   struct tree_t *tgq;
-//   for(;;) {
-//     void *lp = tgp->lock; // initial lp acquired in delete
-//     p = tgp->t;
-//     tgq = p->right;
-//     void *lq = tgq->lock;
-//     acquire(lq);
-//     q = tgq->t;
-//     if (q==NULL) {
-//       struct tree_t *tgl = p->left;
-//       acquire(tgl->lock);
-//       tgp->t = tgl->t;
-//       free(p);
-//       freelock2(lq);
-//       free(lq);
-//       free(tgq);
-//       freelock2(tgl->lock);
-//       free(tgl->lock);
-//       free(tgl);
-//       release2(lp);
-//       return;
-//     } else {
-//       turn_left(tgp, tgq);
-//       tgp = q->left;
-//       release2(lp);
-//     }
-//   }
-// }
-
-// void delete (treebox t, int x) {
-//   struct tree *p;
-//   struct tree_t *tgt;
-//   tgt = *t;
-//   void *l = tgt->lock;
-//   acquire(l);
-//   for(;;) {
-//     p = tgt->t;
-//     if (p==NULL) {
-//       release2(l);
-//       return;
-//     } else {
-//       int y = p->key;
-//       if (x<y){
-// 	tgt = p->left;
-// 	void *l_old = l;
-// 	l = tgt->lock;
-// 	acquire(l);
-//         release2(l_old);
-//       }else if (y<x){
-// 	tgt = p->right;
-// 	void *l_old = l;
-// 	l = tgt->lock;
-// 	acquire(l);
-//         release2(l_old);
-//       }else {
-//       	pushdown_left(tgt);	
-//       	return;
-//       }
-//     }
-//   }
-// }
-
-void *thread_func(void *args) {
-  //lock_t *l = &thread_lock;
-  
-  // insert at key 1
-  insert(tb,1,"ONE_FROM_THREAD");
-  
-  //release2((void*)l);
+void *thread_func_insert(void *args) {
+  lock_t *l = (lock_t*)args;
+  for(int i=1; i<=100; i++)
+    {       
+      insert(tb,i, "value"); 
+    }    
+  printf("insert thread done\n"); 
+  release2((void*)l);
+  printf("insert thread release thread lock\n");
+  return (void *)NULL;
+}
+void *thread_func_lookup(void *args) {
+  lock_t *l = (lock_t*)args;
+  for(int i=1; i<=100; i++)
+    {      
+      printf("%s\n", lookup(tb, i)); 
+    }
+  printf("lookup thread done\n");
+  release2((void*)l);
+  printf("lookup thread release thread lock\n");
   return (void *)NULL;
 }
 
 
+
+
 int main (void) {
-  tb = treebox_new();  
- // lock_t *t_lock = &thread_lock;
-  insert(tb,3,"three");
-  insert(tb,1,"one");
-  insert(tb,4,"four");
-  
-  //makelock((void*)t_lock);
-  /* Spawn */
-  spawn((void *)&thread_func, (void *)NULL);
-  
-  //insert at key 1
-  insert(tb,1,"ONE");
-  
-  /*JOIN */
- // acquire((void*)t_lock);  
- // freelock2((void*)t_lock);
-  
-  /* printf("%s\n", lookup(tb, 1)); */
- // treebox_free(tb);
+  tb = treebox_new();
+
+  /* Spwan 5 lookup thread */
+  for(int i=0; i<100; i++)
+   {
+      lock_t *l = &(thread_lock[i]);    
+      makelock((void*)l);
+      spawn((void *)&thread_func_lookup, (void *)l);
+   }
+
+  /* Spwan 5 insert thread */
+   for(int i=100; i<200; i++)
+   { 
+      lock_t *l = &(thread_lock[i]);    
+      makelock((void*)l);
+      spawn((void *)&thread_func_insert, (void *)l);
+   }
+    printf("I am done to spwan all thread here \n");
+   /*JOIN */
+  for(int i = 0; i < N; i++){
+    lock_t *l = &(thread_lock[i]);
+    acquire((void*)l);
+    printf("Successfully acquired lock here \n");  
+    freelock2((void*)l);
+  }  
+  treebox_free(tb);  
+  printf("Everything done here \n"); 
   return 0;
 }
