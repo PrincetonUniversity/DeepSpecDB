eb44f968f93cf35e7cc2ae543c467cac124345b8
William Mansky <mansky1@uic.edu>
2023-05-02 13:01:57 -0500

    lock-coupling insert proof

--- concurrency/CONCUR 2023/proof_lock_insert.c
+++ concurrency/CONCUR 2023/proof_lock_insert.c
@@ -1,25 +1,25 @@
 void insert (node_t **r, int k, void *v){
 		struct pn *pn = (struct pn *) surely_malloc (sizeof *pn);
 		pn->n = *r;
-		$\color{blue} \left\{\begin{array}{l} \texttt{pn} \mapsto (\texttt{p, r}) \ast  \texttt{r->lock} \lockvar \mathsf{R}(\texttt{r}) \ast \treerep\ m \end{array}\right\}$
+		$\color{blue} \left\{\begin{array}{l} \texttt{pn} \mapsto (\texttt{\_, r}) \ast  \nodeboxrep\ \texttt{r} \end{array}\right\}$
 		acquire(pn->n->lock);
-		$\color{blue} \left\{\begin{array}{l}  \texttt{pn} \mapsto (\texttt{p, r}) \ast  \texttt{r->lock} \lockvar \mathsf{R}(\texttt{r}) \ast \mathsf{R}(\texttt{r}) \ast \cdots \end{array}\right\}$
+		$\color{blue} \left\{\begin{array}{l}  \texttt{pn} \mapsto (\texttt{\_, r}) \ast  \nodeboxrep\ \texttt{r} \ast \mathsf{R}(\texttt{r}) \end{array}\right\}$
 		if (traverse(pn, k) == 0){
-		$\color{blue} \left\{\begin{array}{l}  \mathit{res} = \mathsf{false} \ast \texttt{pn} \mapsto (\texttt{p', p'}) \ast \texttt{p'->t} \neq \texttt{NULL} \ast \texttt{p'->lock} \lockvar \mathsf{R}(\texttt{p'}) \ast \mathsf{R}(\texttt{p'}) \ast \texttt{k} \in \mathsf{range}(\texttt{p'}) \ast \cdots \end{array}\right\}$
+		$\color{blue} \left\{\begin{array}{l}  \exists\ \texttt{n'}.\ \texttt{pn} \mapsto (\texttt{n', n'}) \ast \nodeboxrep\ \texttt{n'} \ast \mathsf{node\_contents}(\texttt{n'}, \texttt{k}, \mathsf{v'}, \mathsf{range'}) \end{array}\right\}$
 				pn->p->t->value = v;
-				$\color{blue} \left\{\begin{array}{l}  \mathit{res} = \mathsf{false} \ast \texttt{pn} \mapsto (\texttt{p', p'}) \ast \texttt{p'->lock} \lockvar \mathsf{R}(\texttt{p'}) \ast \texttt{p'->t->value} \mapsto \texttt{v} \ast \cdots \end{array}\right\} \Rrightarrow{\textbf{commit}}$ 
+				$\color{blue} \left\{\begin{array}{l}  \texttt{pn} \mapsto (\texttt{n', n'}) \ast \nodeboxrep\ \texttt{n'} \ast \mathsf{node\_contents}(\texttt{n'}, \texttt{k}, \texttt{v}, \mathsf{range'}) \end{array}\right\}$ 
 				//Linearization point
-				$\color{blue} \left\{\begin{array}{l}  \texttt{pn} \mapsto (\texttt{p', p'}) \ast \texttt{p'->lock} \lockvar \mathsf{R}(\texttt{p'}) \ast \mathsf{R}(\texttt{p'}) \ast \cdots \end{array}\right\}$
 		}
 		else{
-				$\color{blue} \left\{\begin{array}{l}  \mathit{res} =\mathsf{true} \ast \texttt{pn} \mapsto (\texttt{p', p'}) \ast \texttt{p'->t} = \texttt{NULL}  \ast \texttt{p'->lock} \lockvar \mathsf{R}(\texttt{p'}) \ast \mathsf{R}(\texttt{p'}) \ast \texttt{k} \in \mathsf{range}(\texttt{p'}) \ast \cdots \end{array}\right\}$
+				$\color{blue} \left\{\begin{array}{l}  \exists\ \texttt{n'}.\ \texttt{pn} \mapsto (\texttt{n', n'}) \ast \texttt{n'->t} = \texttt{NULL}  \ast \nodeboxrep\ \texttt{n'} \ast \mathsf{node\_contents}(\texttt{n'}, \cdot, \mathsf{range'}) \ast \texttt{k} \in \mathsf{range'} \end{array}\right\}$
 				insertOp(pn, k, v);
-				$\color{blue} \left\{\begin{array}{l}  \mathit{res} =\mathsf{true} \ast \texttt{pn} \mapsto (\texttt{p', p'}) \ast  \texttt{p'->lock} \lockvar \mathsf{R}(\texttt{p'}) \ast \\ \texttt{p'->t}\mapsto (\texttt{k, v', l, r})  \ast \texttt{l} \mapsto \texttt{NULL} \ast  \texttt{r} \mapsto \texttt{NULL} \ast \cdots \end{array}\right\} \Rrightarrow{\textbf{commit}}$ 
+				$\color{blue} \left\{\begin{array}{l} \texttt{pn} \mapsto (\texttt{n', n'}) \ast \nodeboxrep\ \texttt{n'} \ast \mathsf{node\_contents}(\texttt{n'}, \cdot, \mathsf{range'}) \ast \texttt{k} \in \mathsf{range'}\ \ast \\ \texttt{n'->t}\mapsto (\texttt{k, v, l, r})  \ast \texttt{l} \mapsto \texttt{NULL} \ast  \texttt{r} \mapsto \texttt{NULL} \end{array}\right\}$ 
 				//Linearization point
-				$\color{blue} \left\{\begin{array}{l}  \texttt{pn} \mapsto (\texttt{p', p'}) \ast \texttt{p'->lock} \lockvar \mathsf{R}(\texttt{p'}) \ast \mathsf{R}(\texttt{p'}) \ast \cdots \end{array}\right\}$
+				$\color{blue} \left\{\begin{array}{l} \texttt{pn} \mapsto (\texttt{n', n'}) \ast \nodeboxrep\ \texttt{n'} \ast \mathsf{node\_contents}(\texttt{n'}, \texttt{k}, \texttt{v}, \mathsf{range'}) \ast \texttt{k} \in \mathsf{range'} \end{array}\right\}$ 
 		}
+		$\color{blue} \left\{\begin{array}{l} \texttt{pn} \mapsto (\texttt{n', n'}) \ast \nodeboxrep\ \texttt{n'} \ast \mathsf{R}(\texttt{n'}) \end{array}\right\}$ 
 		release(pn->n->lock);
-		$\color{blue} \left\{\begin{array}{l}  \texttt{pn} \mapsto (\texttt{p', p'}) \ast \texttt{p'->lock} \lockvar \mathsf{R}(\texttt{p'}) \ast \cdots \end{array}\right\}$
+		$\color{blue} \left\{\begin{array}{l}  \texttt{pn} \mapsto (\texttt{n', n'}) \ast \nodeboxrep\ \texttt{n'} \end{array}\right\}$
 		free(pn);
-		$\color{blue} \left\{\begin{array}{l}  \texttt{p'->lock} \lockvar \mathsf{R}(\texttt{p'}) \ast \cdots \end{array}\right\}$
+		$\color{blue} \left\{\begin{array}{l}  \nodeboxrep\ \texttt{n'} \end{array}\right\}$
 }
