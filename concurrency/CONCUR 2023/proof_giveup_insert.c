9c7d125098c934c90b424e1ffc448abb95790a63
Shengyi Wang <txyyss@gmail.com>
2023-05-02 21:35:41 -0400

    Changed font in Figure 9.

--- concurrency/CONCUR 2023/proof_giveup_insert.c
+++ concurrency/CONCUR 2023/proof_giveup_insert.c
@@ -1,23 +1,23 @@
 void insert (node_t **r, int k, void *v){
 		struct pn *pn = (struct pn *) surely_malloc (sizeof *pn);
 		pn->n = *r;
-		$\color{blue} \left\{\begin{array}{l} \texttt{pn} \mapsto (\texttt{\_, r}) \ast  \infp(\texttt{r}) \end{array}\right\}$
+		$\color{blue} \left\{\begin{array}{l} \texttt{pn} \mapsto (\texttt{\_}, \texttt{r}) \ast  \infp(\texttt{r}) \end{array}\right\}$
 		if (traverse(pn, k) == 0){
-				$\color{blue} \left\{\begin{array}{l}  \exists\ \texttt{n'}.\ \texttt{pn} \mapsto (\texttt{n', n'}) \ast \infp(\texttt{n'}) \ast \mathsf{node\_contents}(\texttt{n'}, (\texttt{k}, \mathsf{v'}), \mathsf{range'}) \end{array}\right\}$
+				$\color{blue} \left\{\begin{array}{l}  \exists\ n'.\ \texttt{pn} \mapsto (n', n') \ast \infp(n') \ast \mathsf{node\_contents}(n', (\texttt{k}, \mathsf{v'}), \mathsf{range'}) \end{array}\right\}$
 				pn->p->t->value = v;
-				$\color{blue} \left\{\begin{array}{l}  \texttt{pn} \mapsto (\texttt{n', n'}) \ast \nodeboxrep(\texttt{n'}) \ast \mathsf{node\_contents}(\texttt{n'}, (\texttt{k}, \texttt{v}), \mathsf{range'}) \end{array}\right\} \Rrightarrow{\textbf{commit}}$ 
+				$\color{blue} \left\{\begin{array}{l}  \texttt{pn} \mapsto (n', n') \ast \nodeboxrep(n') \ast \mathsf{node\_contents}(n', (\texttt{k}, \texttt{v}), \mathsf{range'}) \end{array}\right\} \Rrightarrow{\textbf{commit}}$ 
 				//Linearization point
 		}
 		else{
-				$\color{blue} \left\{\begin{array}{l}  \exists\ \texttt{n'}.\ \texttt{pn} \mapsto (\texttt{n', n'}) \ast \texttt{n'->t} = \texttt{NULL}  \ast \infp(\texttt{n'}) \ast \mathsf{node\_contents}(\texttt{n'}, \cdot, \mathsf{range'}) \ast \texttt{k} \in \mathsf{range'} \end{array}\right\}$
+				$\color{blue} \left\{\begin{array}{l}  \exists\ n'.\ \texttt{pn} \mapsto (n', n') \ast n'\texttt{->t} = \texttt{NULL}  \ast \infp(n') \ast \mathsf{node\_contents}(n', \cdot, \mathsf{range'}) \ast \texttt{k} \in \mathsf{range'} \end{array}\right\}$
 				insertOp(pn, k, v);
-				$\color{blue} \left\{\begin{array}{l}  \texttt{pn} \mapsto (\texttt{n', n'}) \ast \infp(\texttt{n'}) \ast \mathsf{node\_contents}(\texttt{n'}, \cdot, \mathsf{range'}) \ast \texttt{k} \in \mathsf{range'}\ \ast \\ \texttt{n'->t}\mapsto (\texttt{k, v, l, r})  \ast \texttt{l} \mapsto \texttt{NULL} \ast  \texttt{r} \mapsto \texttt{NULL} \end{array}\right\}$ 
+				$\color{blue} \left\{\begin{array}{l}  \texttt{pn} \mapsto (n', n') \ast \infp(n') \ast \mathsf{node\_contents}(n', \cdot, \mathsf{range'}) \ast \texttt{k} \in \mathsf{range'}\ \ast \\ n'\texttt{->t}\mapsto (\texttt{k}, \texttt{v}, l, r')  \ast l \mapsto \texttt{NULL} \ast  r' \mapsto \texttt{NULL} \end{array}\right\}$ 
 				//Linearization point
-				$\color{blue} \left\{\begin{array}{l} \texttt{pn} \mapsto (\texttt{n', n'}) \ast \infp(\texttt{n'}) \ast \mathsf{node\_contents}(\texttt{n'}, (\texttt{k}, \texttt{v}), \mathsf{range'}) \ast \texttt{k} \in \mathsf{range'} \end{array}\right\}$ 
+				$\color{blue} \left\{\begin{array}{l} \texttt{pn} \mapsto (n', n') \ast \infp(n') \ast \mathsf{node\_contents}(n', (\texttt{k}, \texttt{v}), \mathsf{range'}) \ast \texttt{k} \in \mathsf{range'} \end{array}\right\}$ 
 		}
-		$\color{blue} \left\{\begin{array}{l} \texttt{pn} \mapsto (\texttt{n', n'}) \ast \infp(\texttt{n'}) \ast \mathsf{R}(\texttt{n'}) \end{array}\right\}$ 
+		$\color{blue} \left\{\begin{array}{l} \texttt{pn} \mapsto (n', n') \ast \infp(n') \ast \mathsf{R}(n') \end{array}\right\}$ 
 		release(pn->n->lock);
-		$\color{blue} \left\{\begin{array}{l}  \texttt{pn} \mapsto (\texttt{n', n'}) \ast \infp(\texttt{n'}) \end{array}\right\}$
+		$\color{blue} \left\{\begin{array}{l}  \texttt{pn} \mapsto (n', n') \ast \infp(n') \end{array}\right\}$
 		free(pn);
-		$\color{blue} \left\{\begin{array}{l} \infp (\texttt{n'}) \end{array}\right\}$
+		$\color{blue} \left\{\begin{array}{l} \infp (n') \end{array}\right\}$
 }
