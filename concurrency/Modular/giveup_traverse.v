42d84b04e2019871543fe6cc7fee708619681e8b
Duc-Than Nguyen <dnguye96@uic.edu>
2023-09-17 15:05:41 -0500

    redefined NodeRep

--- concurrency/Modular/giveup_traverse.v
+++ concurrency/Modular/giveup_traverse.v
@@ -4,7 +4,7 @@ Require Import VST.atomics.general_locks.
 Require Import Coq.Sets.Ensembles.
 Require Import bst.puretree.
 Require Import bst.give_up_template.
-Require Import bst.bst_inst.
+(* Require Import bst.bst_inst. *)
 Require Import VST.atomics.verif_lock_atomic.
 Require Import VST.floyd.library.
 
@@ -16,8 +16,57 @@ Definition t_struct_tree_t := Tstruct _node_t noattr. *)
 Definition t_struct_node := Tstruct _node_t noattr.
 Definition t_struct_pn := Tstruct _pn noattr.
 
+Definition number2Z (x : number) : Z :=
+  match x with
+    | Finite_Integer y => y
+    | Neg_Infinity => Int.min_signed
+    | Pos_Infinity => Int.max_signed
+  end.
 
-Definition ghost_ref (g : own.gname) r1 :=
+Inductive enum : Type := Zero | One | Two.
+
+Definition enums x : val :=
+  match x with
+  | Zero => Vint Int.zero
+  | One => Vint Int.one
+  | Two => Vint (Int.repr 2%Z)
+  end.
+
+#[global] Instance enum_inhabited : Inhabitant (enum).
+Proof.
+  unfold Inhabitant; apply Zero.
+Defined.
+
+#[export] Instance pointer_lock : Ghost := discrete_PCM (val * val * range).
+Definition ghost_info : Type := (key * val)%type.
+
+(* This allows the range to be outdated while the ghost_info may be present or absent. *)
+#[export] Instance node_ghost : Ghost := prod_PCM pointer_lock (exclusive_PCM (option ghost_info)).
+Notation node_info := (@G node_ghost).
+
+Definition in_tree (g: gname) (g1 : gname) (pn: val) (lock: val):=
+      ghost_snap (P := gmap_ghost (K := gname)(A := discrete_PCM (val * val)) )
+        ({[g1 := (pn, lock)]}) g.
+
+Lemma in_tree_duplicate g gin pn lock:
+  in_tree g gin pn lock |-- in_tree g gin pn lock * in_tree g gin pn lock.
+Proof. by rewrite - bi.persistent_sep_dup. Qed.
+
+
+Class NodeRep : Type := {
+      node_rep_R : val -> range -> option (option ghost_info) -> gname -> mpred}.
+  Context (NR: NodeRep).
+  Definition node_rep pn g g_current (r : node_info) :=
+    !!(repable_signed (number2Z r.1.2.1) ∧ repable_signed (number2Z r.1.2.2) /\
+         is_pointer_or_null r.1.1.2) &&
+      field_at Ews (t_struct_node) [StructField _t] r.1.1.1 pn *
+      field_at Ews (t_struct_node) [StructField _min] (vint (number2Z (r.1.2.1))) pn * (*min*)
+      field_at Ews (t_struct_node) [StructField _max] (vint (number2Z (r.1.2.2))) pn * (*max*)
+      malloc_token Ews t_struct_node pn * in_tree g g_current pn r.1.1.2 *
+      node_rep_R r.1.1.1 r.1.2 r.2 g.
+
+  Definition node_lock_inv_pred g p gp a := my_half gp Tsh a * node_rep p g gp a.
+  Definition ghost_ref (g : own.gname) r1 :=
   ghost_master1 (P := gmap_ghost (K := gname) (A := discrete_PCM (val * val))) r1 g.
 
 Lemma ghost_snapshot_intree g (s : gmap gname (val * val))(pn : val)(lock: val)(g_in: gname):
@@ -43,24 +92,6 @@ Proof.
  by apply map_singleton_subseteq_l.
 Qed.
 
-(* t_struct_node represents for the generic-struct rather specific-data-structure *)
-Definition node_rep pn g g_current (r : node_info) :=
-  !!(repable_signed (number2Z r.1.2.1) ∧ repable_signed (number2Z r.1.2.2) /\
-    is_pointer_or_null r.1.1.2) &&
-  field_at Ews (t_struct_node) [StructField _t] r.1.1.1 pn *
-  field_at Ews (t_struct_node) [StructField _min] (vint (number2Z (r.1.2.1))) pn * (*min*)
-  field_at Ews (t_struct_node) [StructField _max] (vint (number2Z (r.1.2.2))) pn * (*max*)
-  malloc_token Ews t_struct_tree_t pn * in_tree g g_current pn r.1.1.2 *
-  node_rep_R r.1.1.1 r.1.2 r.2 g.
-
-#[global] Instance gmap_inhabited V : Inhabitant (gmap key V).
-Proof. unfold Inhabitant; apply empty. Defined.
-
-#[global] Instance number_inhabited: Inhabitant number.
-Proof. unfold Inhabitant; apply Pos_Infinity. Defined.
-
-Definition node_lock_inv_pred g p gp a := my_half gp Tsh a * node_rep p g gp a.
-
 Definition ltree p (lock : val) R:=
   EX lsh, !!(field_compatible t_struct_node nil p /\ readable_share lsh) &&
   (field_at lsh t_struct_node [StructField _lock] lock p * inv_for_lock lock R).
@@ -81,7 +112,7 @@ Record mpredList := {
 }.
 
 (* CSSi *)
-Definition ghost_tree_rep (I : list mpredList) (g: gname): mpred :=
+Definition ghost_tree_rep {N: NodeRep} (I : list mpredList) (g: gname): mpred :=
   iter_sepcon (fun (p: mpredList) => ltree p.(pnL) p.(lockL)
                                 (node_lock_inv_pred g p.(pnL) p.(g_inL) p.(NodeL))) I.
 
@@ -154,7 +185,7 @@ Definition CSS (g g_root : gname) (m: gmap key val): mpred :=
           ghost_tree_rep I g * ghost_ref g (find_ghost_set I).
 
 
-Lemma node_conflict_local pn g g_in a b: node_rep pn g g_in a * node_rep pn g g_in b  |-- FF.
+Lemma node_conflict_local {N: NodeRep} pn g g_in a b: node_rep pn g g_in a * node_rep pn g g_in b  |-- FF.
 Proof.
   unfold node_rep.
   iIntros "(((((((_ & H) & _) & _) & _) & _) & _) & ((((((_ & H') & _) & _) & _) & _) & _))".
@@ -166,18 +197,6 @@ Qed.
 Definition extract_mp_fields (m : mpredList) :=
   (g_inL m, pnL m, lockL m, NodeL m).
 
-Record mpredList1 := {
-    a : nat; b : nat;
-}.
-
-Lemma mp_eq : forall (mp : mpredList1),
-  mp = {| a := a mp; b := b mp; |}.
-Proof.
-  intros mp.
-  destruct mp.
-  simpl.
-  now subst.
-Qed.
 
 Lemma find_ghost (I : list mpredList) (pn lock_in : val) (g g_in: gname):
   find_ghost_set I !! g_in = Some (pn, lock_in) -> exists nodeI,
@@ -191,54 +210,40 @@ Proof.
   {
     clearbody S.
     generalize dependent S.
-    induction I as [| mp I']; intros S.
+    induction I as [| mp I']; intros S; simpl in *.
     - by right.
-    - simpl in *.
-      destruct (S !! g_inL mp) eqn: Heq; intros.
+    - destruct (S !! g_inL mp) eqn: Heq; intros.
       + specialize ((IHI' _) H).
         destruct IHI' as [(NodeI & H1) | H2].
         left; auto.
-        exists NodeI. right. auto.
-        right. auto.
+        exists NodeI; auto. auto.
       + specialize ((IHI' _ ) H).
         destruct IHI' as [(NodeI & H1) | H2].
-        left. exists NodeI. right. auto.
+        left. exists NodeI; by right. 
         destruct (g_inL mp =? g_in)%nat eqn: Hg.
         * rewrite Nat.eqb_eq in Hg.
           subst g_in.
           pose proof Heq as HNone.
           apply insert_union_singleton_r with (x:= (pnL mp, lockL mp)) in Heq.
-          rewrite Heq in H2.
-          rewrite lookup_union_r in H2; auto.
+          rewrite Heq lookup_union_r in H2; auto. 
           rewrite lookup_singleton in H2.
           inversion H2; subst.
           left. exists (NodeL mp).
-          left.
-          destruct mp. now subst. (*Why can't we use reflexivity *)
+          left. by destruct mp.
         * rewrite Nat.eqb_neq in Hg.
           pose proof Heq as HNone.
           apply insert_union_singleton_r with (x:= (pnL mp, lockL mp)) in Heq.
-          rewrite Heq in H2.
-          rewrite lookup_union_l in H2; auto.
+          rewrite Heq lookup_union_l in H2; auto.
           by apply lookup_singleton_None.
   }
   destruct H0 as [(NodeI & H1) | H2].
   - eexists; eauto.
-  - replace S with (∅ : gmap gname (val * val)) in H2; eauto.
-    inversion H2.
+  - by replace S with (∅ : gmap gname (val * val)) in H2.
 Qed.
 
-#[global] Instance Inhabitant_mpredList : Inhabitant mpredList
-  := {
-  default := {|
-    (* gL := 0%nat; *) 
-    g_inL := 0%nat; (* Provide a default value for g_inL *)
-    pnL := Vnullptr ; (* Provide a default value for pnL *)
-    lockL := Vnullptr ; (* Provide a default value for lockL *)
-    NodeL := ((Vnullptr, Vnullptr, (Neg_Infinity, Pos_Infinity)), None)
-  |}
-}.
-
+#[global] Instance Inhabitant_mpredList : Inhabitant mpredList := {
+    default := {| g_inL := 0%nat; pnL := Vnullptr; lockL := Vnullptr;
+               NodeL := ((Vnullptr, Vnullptr, (Neg_Infinity, Pos_Infinity)), None)|} }.
 
 Lemma in_tree_inv1 I pn g g_in (lock_in : val) (lk : val):
        in_tree g g_in pn lock_in * (ghost_tree_rep I g * ghost_ref g (find_ghost_set I)) |--
@@ -246,10 +251,7 @@ Lemma in_tree_inv1 I pn g g_in (lock_in : val) (lk : val):
   inv_for_lock lock_in (node_lock_inv_pred g pn g_in a) *
   (inv_for_lock lock_in (node_lock_inv_pred g pn g_in a) -*
    (ghost_tree_rep I g * ghost_ref g (find_ghost_set I)))) &&
-     (EX R, (ltree pn lock_in R) *
-                           (ltree pn lock_in R
-                              -* (ghost_tree_rep I g *
-                                    ghost_ref g (find_ghost_set I)))).
+     (EX R, (ltree pn lock_in R) * (ltree pn lock_in R -* (ghost_tree_rep I g * ghost_ref g (find_ghost_set I)))).
 Proof.
   iIntros "(H1 & (H2 & H3))".
   iPoseProof (node_exist_in_tree with "[$H1 $H3]") as "%".
@@ -318,6 +320,33 @@ Proof.
       iDestruct "H2" as "(H1 & H2)".
       iExists I; iFrame; done.
 Qed.
+
+Program Definition traverse_spec :=
+  DECLARE _traverse
+          ATOMIC TYPE (rmaps.ConstType
+                       (val * val * val * Z  * val * globals * gname * gname))
+          OBJ M INVS ∅
+  WITH b, n, lock, x, v, gv, g, g_root
+  PRE [ tptr t_struct_pn, tint]
+  PROP (and (Z.le (Int.min_signed) x) (Z.le x (Int.max_signed)); is_pointer_or_null v)
+  PARAMS (b; Vint (Int.repr x)) GLOBALS (gv)
+  SEP  (mem_mgr gv;
+        in_tree g g_root n lock;
+        !!(is_pointer_or_null lock /\ is_pointer_or_null n) && seplog.emp;
+        EX (p: val), data_at Ews (t_struct_pn) (p, n) b ) | (CSS g g_root M)
+  POST [ tint ]
+  EX  pt: enum * (val * (share * gname )) %type,
+  PROP () (* pt: enum * (val * (share * (gname * node_info))) *)
+  LOCAL (temp ret_temp (enums pt.1))
+  SEP (mem_mgr gv)| (CSS g g_root M).
+
+(* t_struct_node represents for the generic-struct rather specific-data-structure *)
+
+#[global] Instance gmap_inhabited V : Inhabitant (gmap key V).
+Proof. unfold Inhabitant; apply empty. Defined.
+
+#[global] Instance number_inhabited: Inhabitant number.
+Proof. unfold Inhabitant; apply Pos_Infinity. Defined.
 
 (*
 
@@ -397,25 +426,9 @@ Definition inRange_spec :=
 
 (*Definition spawn_spec := DECLARE _spawn spawn_spec. *)
 
+Check CSS .
+Context `{N: NodeRep}.
 
-Program Definition traverse_spec :=
-  DECLARE _traverse
-          ATOMIC TYPE (rmaps.ConstType
-                       (val * val * val * Z  * val * globals * gname * gname))
-          OBJ M INVS ∅
-  WITH b, n, lock, x, v, gv, g, g_root
-  PRE [ tptr t_struct_pn, tint]
-  PROP (and (Z.le (Int.min_signed) x) (Z.le x (Int.max_signed)); is_pointer_or_null v)
-  PARAMS (b; Vint (Int.repr x)) GLOBALS (gv)
-  SEP  (mem_mgr gv;
-        in_tree g g_root n lock;
-        !!(is_pointer_or_null lock /\ is_pointer_or_null n) && seplog.emp;
-        EX (p: val), data_at Ews (t_struct_pn) (p, n) b ) | (CSS g g_root M)
-  POST [ tint ]
-  EX  pt: Z * (val * (share * gname )) %type,
-  PROP () (* pt: bool * (val * (share * (gname * node_info))) *)
-  LOCAL (temp ret_temp (Vint (Int.repr pt.1)))
-  SEP (mem_mgr gv)| (CSS g g_root M).
 
 Definition Gprog : funspecs :=
     ltac:(with_library prog [acquire_spec; release_spec; makelock_spec; 
@@ -492,12 +505,15 @@ Proof.
   (* New pt: bool * (val * (share * (gname * node_info))) *)
   forward_loop (traverse_inv b n n Ews x v g_root lock gv inv_names g AS)
     break: (*consider to remove gsh *)
-    (EX (stt: Z) (q : val) (gsh: share) (g_in: gname) (r: node_info),
-     PROP() LOCAL(temp _status (Vint(Int.repr stt)))
-     SEP((if (stt =? 0) then ((traverse_inv_1 b q Ews x g_root g_in g r) *
+    (EX (stt: enum) (q : val) (gsh: share) (g_in: gname) (r: node_info),
+     PROP() LOCAL(temp _status (enums stt))
+     SEP((match stt with
+            | Zero => ((traverse_inv_1 b q Ews x g_root g_in g r) *
                       (!!(r.1.1.1 = nullval) && seplog.emp))
-                 else ((traverse_inv_2 b q Ews x g_root g_in g r) *
-                      (!!( r.1.1.1 <> nullval) && seplog.emp))) *
+            | One => ((traverse_inv_1 b q Ews x g_root g_in g r) *
+                      (!!(r.1.1.1 = nullval) && seplog.emp))
+            | Two => ((traverse_inv_2 b q Ews x g_root g_in g r) *
+                      (!!( r.1.1.1 <> nullval) && seplog.emp)) end) *
                        Q (stt, (q, (gsh, g_in))) * mem_mgr gv)).
   - unfold traverse_inv.
     Exists n p g_root lock. sep_apply in_tree_duplicate. entailer !.
