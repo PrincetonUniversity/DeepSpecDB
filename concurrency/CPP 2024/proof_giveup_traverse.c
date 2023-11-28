0d96c45cdbe46d8561a09081eca09fdc4f0204b5
William Mansky <mansky1@uic.edu>
2023-11-28 14:13:31 -0600

    distinguished nodeboxrep/Ref from in_tree/inFP
    I'd somehow collapsed the two in my descriptions.

--- concurrency/CPP 2024/proof_giveup_traverse.c
+++ concurrency/CPP 2024/proof_giveup_traverse.c
@@ -1,41 +1,41 @@
 int traverse(pn *pn, int k){
 	node_t *r = (pn->n); $\color{specblue} \left\{\begin{array}{l}  \texttt{pn} \mapsto (\texttt{\_, r}) \ast  
-				\infp (\texttt{r}) \end{array}\right\} \Rrightarrow \left\{\begin{array}{l} \mathsf{traverse\_inv} \end{array}\right\}$
+				\inFP (\texttt{r}) \end{array}\right\} \Rrightarrow \left\{\begin{array}{l} \mathsf{traverse\_inv} \end{array}\right\}$
 	for( ; ; ){ 			$\color{specblue} \left\{\begin{array}{l} \mathsf{traverse\_inv} \end{array}\right\} \triangleq \left\{\begin{array}{l} 
-		\exists\ n'.\ \texttt{pn} \mapsto (\texttt{\_}, n') \ast \infp (n') \ast \infp (\texttt{r})
+		\exists\ n'.\ \texttt{pn} \mapsto (\texttt{\_}, n') \ast \inFP (n') \ast \inFP (\texttt{r})
 		 \end{array}\right\}$
 		acquire(pn->n->lock);
-		$\color{specblue} \left\{\begin{array}{l} \texttt{pn} \mapsto (\texttt{\_}, n') \ast \infp (n') \ast \mathsf{R}(n') \ast \infp (\texttt{r}) \end{array}\right\}$
-		pn->p = pn->n; $\color{specblue} \left\{\begin{array}{l} \texttt{pn} \mapsto (n', n') \ast \infp (n') \ast \mathsf{R}(n') \ast \infp (\texttt{r}) \end{array}\right\}$
+		$\color{specblue} \left\{\begin{array}{l} \texttt{pn} \mapsto (\texttt{\_}, n') \ast \inFP (n') \ast \mathsf{R}(n') \ast \inFP (\texttt{r}) \end{array}\right\}$
+		pn->p = pn->n; $\color{specblue} \left\{\begin{array}{l} \texttt{pn} \mapsto (n', n') \ast \inFP (n') \ast \mathsf{R}(n') \ast \inFP (\texttt{r}) \end{array}\right\}$
 		if (inRange(pn, k) == 1){
-			$\color{specblue} \left\{\begin{array}{l} \texttt{k} \in \mathsf{range} \ast \texttt{pn} \mapsto (n', n') \ast \infp (n') \ast \mathsf{node\_contents}(n', c, \mathsf{range}) \ast \infp (\texttt{r}) \end{array}\right\}$
+			$\color{specblue} \left\{\begin{array}{l} \texttt{k} \in \mathsf{range} \ast \texttt{pn} \mapsto (n', n') \ast \inFP (n') \ast \mathsf{node\_contents}(n', c, \mathsf{range}) \ast \inFP (\texttt{r}) \end{array}\right\}$
 			if (pn->p->t == NULL)
 				return 1;
-				$\color{purple!40!black} \left\{\begin{array}{l} \texttt{k} \in \mathsf{range} \ast \texttt{pn} \mapsto (n', n') \ast \infp (n') \ast \mathsf{node\_contents}(n', \cdot, \mathsf{range}) \end{array}\right\}$
+				$\color{purple!40!black} \left\{\begin{array}{l} \texttt{k} \in \mathsf{range} \ast \texttt{pn} \mapsto (n', n') \ast \inFP (n') \ast \mathsf{node\_contents}(n', \cdot, \mathsf{range}) \end{array}\right\}$
 			else{
-				$\color{specblue} \left\{\begin{array}{l} \texttt{k} \in \mathsf{range} \ast \texttt{pn} \mapsto (n', n') \ast \infp (n') \ast \mathsf{node\_contents}(n', (k', v'), \mathsf{range}) \ast \infp (\texttt{r}) \end{array}\right\}$
+				$\color{specblue} \left\{\begin{array}{l} \texttt{k} \in \mathsf{range} \ast \texttt{pn} \mapsto (n', n') \ast \inFP (n') \ast \mathsf{node\_contents}(n', (k', v'), \mathsf{range}) \ast \inFP (\texttt{r}) \end{array}\right\}$
 				int b = findNext(pn, k);
-				$\color{specblue} \left\{\begin{array}{l} \exists \  n''. \ \texttt{pn} \mapsto (n', n'') \ast \infp (n') \ast  \texttt{k} \in \mathsf{range} \ast \mathsf{node\_contents}(n', (k', v'), \mathsf{range})\ \ast \\\infp (\texttt{r}) \ast
-						((\texttt{b} = 0 \ast k' = \texttt{k} \ast n'' = n')  \lor 						 (\texttt{b} = 1  \ast \infp (n'')))   \end{array}\right\}$
+				$\color{specblue} \left\{\begin{array}{l} \exists \  n''. \ \texttt{pn} \mapsto (n', n'') \ast \inFP (n') \ast  \texttt{k} \in \mathsf{range} \ast \mathsf{node\_contents}(n', (k', v'), \mathsf{range})\ \ast \\\inFP (\texttt{r}) \ast
+						((\texttt{b} = 0 \ast k' = \texttt{k} \ast n'' = n')  \lor 						 (\texttt{b} = 1  \ast \inFP (n'')))   \end{array}\right\}$
 				if (b == 0){
 					$\color{specblue} \left\{\begin{array}{l} 
 								\texttt{pn} \mapsto (n', n') \ast k' = \texttt{k} \ast \cdots    \end{array}\right\}$
 					return 0;
-					$\color{purple!40!black} \left\{\begin{array}{l} \texttt{pn} \mapsto (n', n') \ast \infp (n') \ast  \texttt{k} \in \mathsf{range'} \ast \mathsf{node\_contents}(n', (\texttt{k}, v'), \mathsf{range})   \end{array}\right\}$
+					$\color{purple!40!black} \left\{\begin{array}{l} \texttt{pn} \mapsto (n', n') \ast \inFP (n') \ast  \texttt{k} \in \mathsf{range'} \ast \mathsf{node\_contents}(n', (\texttt{k}, v'), \mathsf{range})   \end{array}\right\}$
 				}
 				else{
-					$\color{specblue} \left\{\begin{array}{l} \texttt{pn} \mapsto (n', n'') \ast \infp (n') \ast  \texttt{k} \in \mathsf{range} \ast \mathsf{node\_contents}(n',(k', v'), \mathsf{range})\ \ast\\ \infp (n'') \ast \infp (\texttt{r})   \end{array}\right\}$
+					$\color{specblue} \left\{\begin{array}{l} \texttt{pn} \mapsto (n', n'') \ast \inFP (n') \ast  \texttt{k} \in \mathsf{range} \ast \mathsf{node\_contents}(n',(k', v'), \mathsf{range})\ \ast\\ \inFP (n'') \ast \inFP (\texttt{r})   \end{array}\right\}$
 					release(pn->p->lock);
-					$\color{specblue} \left\{\begin{array}{l} \texttt{pn} \mapsto (n', n'') \ast \infp (n') \ast  \texttt{k} \in \mathsf{range} \ast \infp (n'') \ast \infp (\texttt{r})\end{array}\right\}$
+					$\color{specblue} \left\{\begin{array}{l} \texttt{pn} \mapsto (n', n'') \ast \inFP (n') \ast  \texttt{k} \in \mathsf{range} \ast \inFP (n'') \ast \inFP (\texttt{r})\end{array}\right\}$
 				}
 			}
 		}
 		else{
-			$\color{specblue} \left\{\begin{array}{l} \texttt{pn} \mapsto (n', n') \ast \infp (n') \ast \mathsf{R}(n') \ast \infp (\texttt{r})\end{array}\right\}$
+			$\color{specblue} \left\{\begin{array}{l} \texttt{pn} \mapsto (n', n') \ast \inFP (n') \ast \mathsf{R}(n') \ast \inFP (\texttt{r})\end{array}\right\}$
 			release(pn->p->lock);
-			$\color{specblue} \left\{\begin{array}{l} \texttt{pn} \mapsto (n', n') \ast \infp (n') \ast \infp (\texttt{r}) \end{array}\right\}$
+			$\color{specblue} \left\{\begin{array}{l} \texttt{pn} \mapsto (n', n') \ast \inFP (n') \ast \inFP (\texttt{r}) \end{array}\right\}$
 			pn->n = r;
-			$\color{specblue} \left\{\begin{array}{l} \texttt{pn} \mapsto (n', \texttt{r}) \ast \infp (\texttt{r})   \end{array}\right\}$
+			$\color{specblue} \left\{\begin{array}{l} \texttt{pn} \mapsto (n', \texttt{r}) \ast \inFP (\texttt{r})   \end{array}\right\}$
 		}
 	}
 }
--- concurrency/CPP 2024/proof_lock_traverse.c
+++ concurrency/CPP 2024/proof_lock_traverse.c
@@ -1,30 +1,30 @@
 int traverse(pn *pn, int k){
 	$\color{specblue} \left\{\begin{array}{l}  \texttt{pn} \mapsto (p, n) \ast  
-				\nodeboxrep(n)  \ast \mathsf{R}(n) 		\end{array}\right\} \Rrightarrow \left\{\begin{array}{l} \mathsf{traverse\_inv} \end{array}\right\}$
+				\inFP(n)  \ast \mathsf{R}(n) 		\end{array}\right\} \Rrightarrow \left\{\begin{array}{l} \mathsf{traverse\_inv} \end{array}\right\}$
 	for( ; ; ){ 			$\color{specblue} \left\{\begin{array}{l} \mathsf{traverse\_inv} \end{array}\right\} $
 		pn->p = pn->n;
-		$\color{specblue} \left\{\begin{array}{l} \texttt{pn} \mapsto (n', n') \ast  \nodeboxrep(n') \ast  \texttt{k} \in \mathsf{range} \ast \mathsf{node\_contents}(n', c, \mathsf{range})\end{array}\right\}$
+		$\color{specblue} \left\{\begin{array}{l} \texttt{pn} \mapsto (n', n') \ast  \inFP(n') \ast  \texttt{k} \in \mathsf{range} \ast \mathsf{node\_contents}(n', c, \mathsf{range})\end{array}\right\}$
 		if (pn->p->t == NULL)
 			return 1;						
-			$\color{purple!40!black} \left\{\begin{array}{l} \texttt{pn} \mapsto (n', n') \ast \nodeboxrep(n') \ast  \texttt{k} \in \mathsf{range} \ast \mathsf{node\_contents}(n', \cdot, \mathsf{range}) \end{array}\right\}$
+			$\color{purple!40!black} \left\{\begin{array}{l} \texttt{pn} \mapsto (n', n') \ast \inFP(n') \ast  \texttt{k} \in \mathsf{range} \ast \mathsf{node\_contents}(n', \cdot, \mathsf{range}) \end{array}\right\}$
 		else{
-			$\color{specblue} \left\{\begin{array}{l} \texttt{pn} \mapsto (n', n') \ast  \nodeboxrep(n') \ast  \texttt{k} \in \mathsf{range} \ast \mathsf{node\_contents}(n', (k', v'), \mathsf{range})\end{array}\right\}$
+			$\color{specblue} \left\{\begin{array}{l} \texttt{pn} \mapsto (n', n') \ast  \inFP(n') \ast  \texttt{k} \in \mathsf{range} \ast \mathsf{node\_contents}(n', (k', v'), \mathsf{range})\end{array}\right\}$
 			int b = findNext(pn, k);
-			$\color{specblue} \left\{\begin{array}{l} \exists \  n''. \ \texttt{pn} \mapsto (n', n'') \ast \nodeboxrep(n') \ast  \texttt{k} \in \mathsf{range} \ast \mathsf{node\_contents}(n', (k', v'), \mathsf{range})\ \ast\\ 
-						((\texttt{b} = 0 \ast k' = \texttt{k} \ast n'' = n')  \lor 						 (\texttt{b} = 1  \ast \texttt{k} \in \mathsf{range}(n'') \ast \nodeboxrep(n'')))   \end{array}\right\}$
+			$\color{specblue} \left\{\begin{array}{l} \exists \  n''. \ \texttt{pn} \mapsto (n', n'') \ast \inFP(n') \ast  \texttt{k} \in \mathsf{range} \ast \mathsf{node\_contents}(n', (k', v'), \mathsf{range})\ \ast\\ 
+						((\texttt{b} = 0 \ast k' = \texttt{k} \ast n'' = n')  \lor 						 (\texttt{b} = 1  \ast \texttt{k} \in \mathsf{range}(n'') \ast \inFP(n'')))   \end{array}\right\}$
 			if (b == 0){
 				$\color{specblue} \left\{\begin{array}{l} 
 								\texttt{pn} \mapsto (n', n') \ast k' = \texttt{k} \ast \cdots    \end{array}\right\}$
 				return 0;
 				$\color{purple!40!black} \left\{\begin{array}{l} 
-								\texttt{pn} \mapsto (n', n') \ast \nodeboxrep(n') \ast  \texttt{k} \in \mathsf{range'} \ast \mathsf{node\_contents}(n', (\texttt{k}, v'), \mathsf{range'})    \end{array}\right\}$
+								\texttt{pn} \mapsto (n', n') \ast \inFP(n') \ast  \texttt{k} \in \mathsf{range'} \ast \mathsf{node\_contents}(n', (\texttt{k}, v'), \mathsf{range'})    \end{array}\right\}$
 			}
 			else{
-				$\color{specblue} \left\{\begin{array}{l} \texttt{pn} \mapsto (n', n'') \ast \nodeboxrep(n') \ast  \texttt{k} \in \mathsf{range}(n'') \ast \mathsf{R}(n') \ast \nodeboxrep(n'')    \end{array}\right\}$
+				$\color{specblue} \left\{\begin{array}{l} \texttt{pn} \mapsto (n', n'') \ast \inFP(n') \ast  \texttt{k} \in \mathsf{range}(n'') \ast \mathsf{R}(n') \ast \inFP(n'')    \end{array}\right\}$
 				acquire(pn->n->lock);
-				$\color{specblue} \left\{ \begin{array}{l}\texttt{pn} \mapsto (n', n'') \ast \nodeboxrep(n') \ast  \texttt{k} \in \mathsf{range}(n'') \ast \mathsf{R}(n') \ast \nodeboxrep(n'') \ast \mathsf{R}(n'')\end{array}\right\}$
+				$\color{specblue} \left\{ \begin{array}{l}\texttt{pn} \mapsto (n', n'') \ast \inFP(n') \ast  \texttt{k} \in \mathsf{range}(n'') \ast \mathsf{R}(n') \ast \inFP(n'') \ast \mathsf{R}(n'')\end{array}\right\}$
 				release(pn->p->lock);
-				$\color{specblue} \left\{\begin{array}{l} \texttt{pn} \mapsto (n', n'') \ast \texttt{k} \in \mathsf{range}(n'') \ast \nodeboxrep(n'') \ast \mathsf{R}(n'')    \end{array}\right\}$
+				$\color{specblue} \left\{\begin{array}{l} \texttt{pn} \mapsto (n', n'') \ast \texttt{k} \in \mathsf{range}(n'') \ast \inFP(n'') \ast \mathsf{R}(n'')    \end{array}\right\}$
 			}
 		}
 	}
