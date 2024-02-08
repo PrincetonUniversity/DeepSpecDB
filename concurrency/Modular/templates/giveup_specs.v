214dbe708dbd972f3358f2fd7a3bcd996178d2d2
ducthann <dnguye96@uic.edu>
2024-02-08 15:19:38 -0600

    more progress giving specs for giveup_specs

--- concurrency/Modular/templates/giveup_specs.v
+++ concurrency/Modular/templates/giveup_specs.v
@@ -19,6 +19,7 @@ Proof. unfold Inhabitant; apply Pos_Infinity. Defined.
 
 Section give_up_specs.
   Context {N: NodeRep}.
+  
 Definition nodebox_rep (g : gname) (g_root : gname) (sh : share) (lock: val) (nb : val) :=
   EX (np: val) (lsh : share),
                 data_at sh (tptr (t_struct_node)) np nb *
@@ -199,10 +200,9 @@ Definition findnext_spec :=
   PRE [ tptr t_struct_nodeds, tptr (tptr tvoid), tint ]
           PROP (writable_share sh(*; is_pointer_or_null pa; is_pointer_or_null pb*) )
           PARAMS (p; n; Vint (Int.repr x)) GLOBALS (gv)
-          SEP (node_rep_R r.1.1.1 r.1.2 r.2 g * (!!(p = r.1.1.1) && seplog.emp) *
+          SEP (node_rep_R r.1.1.1 r.2 g * (!!(p = r.1.1.1 /\ p <> nullval) && seplog.emp) *
                (* field_at sh (t_struct_tree) [StructField _t] r.1.1.1 p; *)
-               
-               data_at sh (tptr t_struct_node) n_pt n)
+               data_at sh (tptr tvoid) n_pt n)
   POST [ tint ]
   EX (stt: enum), EX (n' next : val),
          PROP (match stt with
@@ -211,206 +211,124 @@ Definition findnext_spec :=
                end)
         LOCAL (temp ret_temp (enums stt))
         SEP (match stt with
-               | F | NF => data_at sh (tptr t_struct_node) n_pt n
-               | NN => !!(n' ∈ extract_node_pn r) && data_at sh (tptr t_struct_node) next n
+               | F | NF => data_at sh (tptr tvoid) n_pt n
+               | NN => !!(n' ∈ extract_node_pn r) && data_at sh (tptr tvoid) next n
              end *
-               node_rep_R r.1.1.1 r.1.2 r.2 g) .
-(*
-Definition insertOp_spec :=
-  DECLARE _insertOp
-    WITH x: Z, stt: Z, v: val, p: val, p1: val, p2: val, r: node_info,
-                    g: gname, gv: globals
-  PRE [ tptr (tptr t_struct_nodeds), tint, tptr tvoid, tint, tptr tvoid, tptr tvoid]
-  PROP (repable_signed x; is_pointer_or_null v; key_in_range x r.1.2 = true )
-  PARAMS (p; Vint (Int.repr x); v; Vint (Int.repr stt); p1; p2)
-  GLOBALS (gv)
-  SEP (mem_mgr gv; node_rep_R nullval r.1.2 r.2 g;
-       data_at Ews (tptr t_struct_nodeds) nullval p)
-  POST[ tvoid ]
-  EX (pnt : val),
-  PROP (pnt <> nullval)
-  LOCAL ()
-  SEP (mem_mgr gv; node_rep_R pnt r.1.2 (Some (Some (x, v, [p1; p2]))) g;
-       data_at Ews (tptr t_struct_nodeds) pnt p).
+               node_rep_R r.1.1.1 r.2 g) .
 
-Definition fst_list : list (val * val * val * val) -> list val :=
-  fun triples => map (fun '(x, _, _,_) => x) triples.
+(** Insert giveup spec **)
 
-Definition snd_list : list (val * val * val * val) -> list val :=
-  fun triples => map (fun '(_, x, _,_) => x) triples.
-
+Definition fst_list : list (val * val * val * val) -> list val := map (fun '(x, _, _,_) => x).
+Definition snd_list : list (val * val * val * val) -> list val := map (fun '(_, x, _,_) => x).
 Definition thrd_frth_list: list (val * val * val * val) -> list (val * val) :=
-  fun triples => map (fun '(_, _, x, y) => (x, y)) triples.
-
-
+  map (fun '(_, _, x, y) => (x, y)).
 Definition fst_snd_list: list (val * val * val * val) -> list (val * val) :=
-  fun triples => map (fun '(x, y, _,_) => (x, y)) triples.
-
+  map (fun '(x, y, _,_) => (x, y)).
 (*getting p that points to min *)
-
 Definition fst_thrd_list: list (val * val * val * val) -> list (val * val) :=
-  fun triples => map (fun '(x, _, y,_) => (x, y)) triples.
-
+  map (fun '(x, _, y,_) => (x, y)).
 (*getting p that points to max *)
 Definition fst_frth_list: list (val * val * val * val) -> list (val * val) :=
-  fun triples => map (fun '(x, _, _, y) => (x, y)) triples.
-
-Check fst_snd_list.
+  map (fun '(x, _, _, y) => (x, y)).
 
 
-Definition insertOp_bst_spec :=
-  DECLARE _insertOp_bst
-    WITH x: Z, stt: Z,  v: val, p: val, tp: val, min: Z, max: Z, r: node_info, g: gname, gv: globals
+Definition post_insert_giveup1 (p pnt : val) (x: Z) v
+  min max (trl : list (val * val * val * val)) g :=
+       iter_sepcon (fun pn => malloc_token Ews t_struct_node pn) (fst_list trl) * 
+       iter_sepcon (fun pn => field_at Ews t_struct_node (DOT _t) (Vlong (Int64.repr 0)) pn)
+         (fst_list trl) * 
+       iter_sepcon (fun lk => atomic_int_at Ews (vint 0) lk) (snd_list trl) * 
+       iter_sepcon (fun fs => field_at Ews t_struct_node (DOT _lock) (snd fs) (fst fs))
+         (fst_snd_list trl) * 
+       iter_sepcon (fun ft => field_at Ews t_struct_node (DOT _min) (snd ft) (fst ft))
+         (fst_thrd_list trl) * 
+       iter_sepcon (fun ff => field_at Ews t_struct_node (DOT _max) (snd ff) (fst ff))
+         (fst_frth_list trl) * 
+       node_rep_R pnt (Some (Some (x, v, (fst_list trl)))) g *
+       field_at Ews t_struct_node (DOT _t) pnt p *
+       field_at Ews t_struct_node (DOT _min) (vint min) p * 
+       field_at Ews t_struct_node (DOT _max) (vint max) p.
+
+Definition post_insert_giveup2 (p pnt tp: val) (x: Z) v 
+  min max (trl : list (val * val * val * val)) (r: node_info) g :=
+       iter_sepcon (fun pn => malloc_token Ews t_struct_node pn) (fst_list trl) * 
+       iter_sepcon (fun pn => field_at Ews t_struct_node (DOT _t) tp pn)
+         (fst_list trl) * 
+       iter_sepcon (fun lk => atomic_int_at Ews (vint 0) lk) (snd_list trl) * 
+       iter_sepcon (fun fs => field_at Ews t_struct_node (DOT _lock) (snd fs) (fst fs))
+         (fst_snd_list trl) * 
+       iter_sepcon (fun ft => field_at Ews t_struct_node (DOT _min) (snd ft) (fst ft))
+         (fst_thrd_list trl) * 
+       iter_sepcon (fun ff => field_at Ews t_struct_node (DOT _max) (snd ff) (fst ff))
+         (fst_frth_list trl) * 
+       node_rep_R tp r.2 g * node_rep_R pnt (Some (Some (x, v, (fst_list trl)))) g *
+       field_at Ews t_struct_node (DOT _t) pnt p * 
+       field_at Ews t_struct_node (DOT _min) (vint min) p * 
+         field_at Ews t_struct_node (DOT _max) (vint max) p.
+
+Definition insertOp_giveup_spec :=
+  DECLARE _insertOp_giveup
+    WITH x: Z, stt: Z, v: val, p: val, tp: val, min: Z, max: Z, r: node_info, g: gname, gv: globals
   PRE [ tptr t_struct_node, tint, tptr tvoid, tint ]
   PROP (repable_signed min; repable_signed max; repable_signed x;
-        is_pointer_or_null v; key_in_range x r.1.2 = true; tp = nullval )
+        is_pointer_or_null v; is_pointer_or_null v; is_pointer_or_null tp)
   PARAMS (p; Vint (Int.repr x); v; Vint (Int.repr stt))
   GLOBALS (gv)
   SEP (mem_mgr gv;
+       match (Int.eq (Int.repr stt) (Int.repr 2%Z)) with
+        | true => (!!(tp = nullval /\ (r.2 = Some None) ) && seplog.emp)
+        | false => (!!(tp <> nullval) && seplog.emp)
+       end;
        field_at Ews t_struct_node (DOT _t) tp p;
        field_at Ews t_struct_node (DOT _min) (vint min) p;
        field_at Ews t_struct_node (DOT _max) (vint max) p;
-       node_rep_R tp r.1.2 r.2 g )
+       node_rep_R tp r.2 g )
   POST[ tvoid ] (* triple (pointer, lock, min, max) *)
   EX (pnt : val) (trl : list (val * val * val * val)),
   PROP (pnt <> nullval)
   LOCAL ()
   SEP (mem_mgr gv;
-       field_at Ews t_struct_node (DOT _t) pnt p;
-       (* malloc_token Ews t_struct_nodeds pnt; *)
-       iter_sepcon (fun pn => malloc_token Ews t_struct_node pn) (fst_list trl);
-       iter_sepcon (fun pn => field_at Ews t_struct_node (DOT _t) (Vlong (Int64.repr 0)) pn)
-         (fst_list trl);
-       iter_sepcon (fun lk => atomic_int_at Ews (vint 0) lk) (snd_list trl);
-       iter_sepcon (fun fs => field_at Ews t_struct_node (DOT _lock) (snd fs) (fst fs))
-         (fst_snd_list trl);
-       iter_sepcon (fun ft => field_at Ews t_struct_node (DOT _min) (snd ft) (fst ft))
-         (fst_thrd_list trl);
-       iter_sepcon (fun ff => field_at Ews t_struct_node (DOT _max) (snd ff) (fst ff))
-         (fst_frth_list trl);
-       node_rep_R pnt r.1.2 (Some (Some (x, v, (fst_list trl)))) g;
-       field_at Ews t_struct_node (DOT _min) (vint min) p;
-       field_at Ews t_struct_node (DOT _max) (vint max) p).
+       (match (Int.eq (Int.repr stt) (Int.repr 2%Z)) with
+        | true => (post_insert_giveup1 p pnt x v min max trl g)
+        | _    => (post_insert_giveup2 p pnt tp x v min
+                   max trl r g)
+        end)).
 
 
-Definition insertOp_bst_spec :=
-  DECLARE _insertOp_bst
-    WITH x: Z, stt: Z,  v: val, p: val, tp: val, min: Z, max: Z, r: node_info, g: gname, gv: globals
-  PRE [ tptr t_struct_node, tint, tptr tvoid, tint ]
-  PROP (repable_signed min; repable_signed max; repable_signed x;
-        is_pointer_or_null v; key_in_range x r.1.2 = true; tp = nullval )
-  PARAMS (p; Vint (Int.repr x); v; Vint (Int.repr stt))
-  GLOBALS (gv)
-  SEP (mem_mgr gv;
-       field_at Ews t_struct_node (DOT _t) tp p;
-       field_at Ews t_struct_node (DOT _min) (vint min) p;
-       field_at Ews t_struct_node (DOT _max) (vint max) p;
-       node_rep_R tp r.1.2 r.2 g )
-  POST[ tvoid ]
-  EX (pnt : val) (pnl: list val) (lkl: list val) (rng : list (val * val)),
-  PROP (pnt <> nullval)
-  LOCAL ()
-  SEP (mem_mgr gv;
-       (* data_at sh t_struct_pn (p, p) b; *)
-       field_at Ews t_struct_node (DOT _t) pnt p;
-       (* malloc_token Ews t_struct_nodeds pnt; *)
-       iter_sepcon (fun pn => malloc_token Ews t_struct_node pn) pnl;
-       iter_sepcon (fun lk => atomic_int_at Ews (vint 0) lk) lkl;
-       iter_sepcon (fun pair => field_at Ews t_struct_node (DOT _lock) (fst pair) (snd pair)) pair; 
-       (* iter_sepcon (fun pn lk rg => data_at Ews t_struct_node (Vlong (Int64.repr 0), (lk, rg)) pn) pnl lkl rng
-       ; *)
-       node_rep_R pnt r.1.2 (Some (Some (x, v, pnl))) g;
-(*
-       data_at Ews t_struct_tree (vint x, (v, (p1', p2'))) pnt;
-       data_at Ews t_struct_tree_t (Vlong (Int64.repr 0), (lock2, (vint x, vint max))) p2';
-       data_at Ews t_struct_tree_t (Vlong (Int64.repr 0), (lock1, (vint min, vint x))) p1';
-*)
-       field_at Ews t_struct_node (DOT _min) (vint min) p;
-       field_at Ews t_struct_node (DOT _max) (vint max) p).
-
-
+(* same as insertOp_giveup_spec, only renaming name *)
 Definition insertOp_helper_spec :=
   DECLARE _insertOp_helper
     WITH x: Z, stt: Z,  v: val, p: val, tp: val, min: Z, max: Z, r: node_info, g: gname, gv: globals
   PRE [ tptr t_struct_node, tint, tptr tvoid, tint ]
   PROP (repable_signed min; repable_signed max; repable_signed x;
-        is_pointer_or_null v; key_in_range x r.1.2 = true; tp = nullval )
+        is_pointer_or_null v; is_pointer_or_null v; is_pointer_or_null tp)
   PARAMS (p; Vint (Int.repr x); v; Vint (Int.repr stt))
   GLOBALS (gv)
   SEP (mem_mgr gv;
+       match (Int.eq (Int.repr stt) (Int.repr 2%Z)) with
+        | true => (!!(tp = nullval /\ (r.2 = Some None) ) && seplog.emp)
+        | false => (!!(tp <> nullval) && seplog.emp)
+       end;
        field_at Ews t_struct_node (DOT _t) tp p;
        field_at Ews t_struct_node (DOT _min) (vint min) p;
        field_at Ews t_struct_node (DOT _max) (vint max) p;
-       node_rep_R tp r.1.2 r.2 g )
+       node_rep_R tp r.2 g )
   POST[ tvoid ] (* triple (pointer, lock, min, max) *)
   EX (pnt : val) (trl : list (val * val * val * val)),
   PROP (pnt <> nullval)
   LOCAL ()
   SEP (mem_mgr gv;
-       field_at Ews t_struct_node (DOT _t) pnt p;
-       (* malloc_token Ews t_struct_nodeds pnt; *)
-       iter_sepcon (fun pn => malloc_token Ews t_struct_node pn) (fst_list trl);
-       iter_sepcon (fun pn => field_at Ews t_struct_node (DOT _t) (Vlong (Int64.repr 0)) pn)
-         (fst_list trl);
-       iter_sepcon (fun lk => atomic_int_at Ews (vint 0) lk) (snd_list trl);
-       iter_sepcon (fun fs => field_at Ews t_struct_node (DOT _lock) (snd fs) (fst fs))
-         (fst_snd_list trl);
-       iter_sepcon (fun ft => field_at Ews t_struct_node (DOT _min) (snd ft) (fst ft))
-         (fst_thrd_list trl);
-       iter_sepcon (fun ff => field_at Ews t_struct_node (DOT _max) (snd ff) (fst ff))
-         (fst_frth_list trl);
-       node_rep_R pnt r.1.2 (Some (Some (x, v, (fst_list trl)))) g;
-       field_at Ews t_struct_node (DOT _min) (vint min) p;
-       field_at Ews t_struct_node (DOT _max) (vint max) p).
+       (match (Int.eq (Int.repr stt) (Int.repr 2%Z)) with
+        | true => (post_insert_giveup1 p pnt x v min max trl g)
+        | _    => (post_insert_giveup2 p pnt tp x v min
+                   max trl r g)
+        end)).
 
 
 Definition Gprog : funspecs :=
-    ltac:(with_library prog [acquire_spec; release_spec; makelock_spec; findnext_spec; 
-                             inRange_spec; traverse_spec; insertOp_bst_spec; insertOp_spec;
+    ltac:(with_library prog [acquire_spec; release_spec; makelock_spec; findnext_spec; insertOp_giveup_spec; 
+                             inRange_spec; traverse_spec; 
                              surely_malloc_spec; insertOp_helper_spec ]).
-
-Lemma insertOp_bst: semax_body Vprog Gprog f_insertOp_bst insertOp_bst_spec.
-Proof.
-  start_function.
-  forward_call (t_struct_node, gv).
-  Intros p1.
-  forward_call (t_struct_node, gv).
-  Intros p2.
-  forward.
-  assert_PROP (field_compatible t_struct_node [] p1) by entailer!.
-  assert_PROP (field_compatible t_struct_node [] p2) by entailer!.
-  forward. 
-  forward_call (gv).
-  Intros lock1.
-  forward.
-  sep_apply atomic_int_isptr.
-  Intros.
-  forward_call release_nonatomic (lock1).
-  forward_call (gv).
-  Intros lock2.
-  forward.
-  Intros.
-  forward_call release_nonatomic (lock2).
-  assert_PROP (field_compatible t_struct_node (DOT _t) p). entailer !. 
-  forward_call(x, stt, v, p, p1, p2, r, g, gv).
-  entailer !.
-  rewrite field_at_data_at'. simpl.
-  entailer !.
-  Intros pnt.
-  forward.
-  forward.
-  forward.
-  forward.
-  forward.
-  forward.
-  Exists pnt.
-  Exists [(p1, lock1, vint min, vint x); (p2, lock2, vint x, vint max)].
-  entailer !.
-  rewrite field_at_data_at'. simpl.
-  unfold_data_at (data_at Ews _  _ p1).
-  unfold_data_at (data_at Ews _  _ p2).
-  entailer !.
-Qed.
 
 Lemma insertOp_helper: semax_body Vprog Gprog f_insertOp_helper insertOp_helper_spec.
 Proof.
@@ -422,5 +340,4 @@ Proof.
   entailer !.
 Qed.
 
-*)
 End give_up_specs.
